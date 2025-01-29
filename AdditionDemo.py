5e14e2bcca6a301d9c70ef6cd42e44814944827d
Alex Warren <WarrenWorkshopMedia@gmail.com>
2025-01-29 13:12:06 -0500

    Update AdditionDemo.py

---
+++ AdditionDemo.py
@@ -0,0 +1,16 @@
+#Alexander Warren CSD-1510 OH01C Python Class
+number1 = 0
+number2 = 0
+
+print("Please enter two numbers")
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+
+sum = number1 + number2
+
+print(f"The sum of {number1} and {number2} is {sum}.")
+
+
+
+
+
