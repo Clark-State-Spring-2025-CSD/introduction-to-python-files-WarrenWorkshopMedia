a361e77a15e90257c8331d8e847ff4ffbf87e8cd
Alex Warren <WarrenWorkshopMedia@gmail.com>
2025-01-30 12:17:17 -0500

    Update AverageAssignment.py

--- AverageAssignment.py
+++ AverageAssignment.py
@@ -3,15 +3,10 @@
 #Display the three numbers entered, their sum, and the average.
 #You can assume the user will only enter positive integers
 #Example output: (you do not have to match my words exactly)
-
 #Please enter three whole numbers:
-
 #Number 1: 11
-
 #Number 2: 11
-
 #Number 3: 12
-
 #The sum of 11 and 11 and 12 is 34 and the average is 11.333333333333334.
 
 Number1 = 0
